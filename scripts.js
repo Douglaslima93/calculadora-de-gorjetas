cbd73b91966645cca1e4122f3cfdcfd72db11a4b
Douglas Lima <douglas07a@gmail.com>
2023-02-25 11:47:51 -0300

    Atualização e inclusão JS v5

---
+++ scripts.js
@@ -0,0 +1,5 @@
+function  insert(totalp){
+var valor = document.getElementById('input-bill')
+var people = document.getElementById('input-people')
+document.getElementById('txt4').innerHTML = valor / people
+}
