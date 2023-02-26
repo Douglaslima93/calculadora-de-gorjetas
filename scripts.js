36a447a26f3d54e797b15e64119da4153a6a3744
Douglas Lima <douglas07a@gmail.com>
2023-02-26 16:27:03 -0300

    Atualização de conteudo v6

--- scripts.js
+++ scripts.js
@@ -1,5 +1,29 @@
-function  insert(totalp){
-var valor = document.getElementById('input-bill')
-var people = document.getElementById('input-people')
-document.getElementById('txt4').innerHTML = valor / people
+var resultadopessoa = document.querySelector('#txt4')
+var resultadototal = document.querySelector('#txt5')
+
+var b5 = document.querySelector('#botao5%')
+var b10 = document.querySelector('#botao10%')
+var b15 = document.querySelector('#botao15%')
+var b25 = document.querySelector('#botao20%')
+var b50 = document.querySelector('##botao50%')
+
+
+function  calcularGor5(){
+
+var valor = document.querySelector('#input-bill')
+var people = document.querySelector('#input-peoplo')
+
+if (input-bill.value !=="" && input-people.value !==""){
+
+    valorGorjetaPessoa = ((input-bill.value/100) * 5)/input-people.value
+    valorTotalpessoa = (input-bill.value/input-people.value) + valorGorjetaPessoa
+
+    resultadopessoa.innerHTML= `\$ ${valorGorjetaPessoa.tofixed(2)}`
+    resultadototal.innerHTML= `\$ ${valorGorjetaPessoa.tofixed(2)}`
+    console.log(input-bill.value)
+
+}else{
+    alert('Preencha todos os campos')
+}
+
 }
